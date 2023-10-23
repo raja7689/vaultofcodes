e421c321335ba2360a6d82f9fd8503aaa567d980
raja7689 <146950593+raja7689@users.noreply.github.com>
2023-10-23 12:30:34 +0530

    Add files via upload

---
+++ task-3/script.js
@@ -0,0 +1,23 @@
+const password = document.getElementById('password');
+const passwordStrength = document.getElementById('password-strength');
+
+password.addEventListener('input', () => {
+    const strength = checkPasswordStrength(password.value);
+    passwordStrength.textContent = `Password Strength: ${strength}`;
+});
+
+function checkPasswordStrength(password) {
+    const strength = {
+        weak: 0,
+        medium: 1,
+        strong: 2
+    };
+
+    if (password.length < 8) {
+        return 'Weak';
+    } else if (password.length < 12) {
+        return 'Medium';
+    } else {
+        return 'Strong';
+    }
+}
